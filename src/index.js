34a2285c23434568c8a25e321a91ac66e3fc3673
DzmiVas <hobotdmitri@gmail.com>
2024-09-09 18:05:53 +0200

    update: create solution for function reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+
+    let i = n;
+      
+    while (i <= reverse && i>0) {
+        console.log(i);
+        i = i - 1;
+    }
+    console.log('finished!');
+    
+    };
+
